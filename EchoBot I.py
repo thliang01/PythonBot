d00fa53deaaa4323d399cdace52d8d3f3bfa69c5
thomas <thliang01@gmail.com>
2020-01-19 23:30:58 +0800

    update echoBot

---
+++ EchoBot I.py
@@ -0,0 +1,14 @@
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+
+# Define a function that responds to a user's message: respond
+def respond(user_template):
+    # Concatenate the user's message to the end of a standard bot respone
+    bot_message = "I can hear you! You said: " + user_template
+    # Return the result
+    return bot_message
+
+
+# Test function
+print(respond("hello!"))
